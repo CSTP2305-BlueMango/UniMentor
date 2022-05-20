20c6c288305a807af872a847de19131ac37efc37
Son Minh Nguyen <000433430@st>
2022-05-19 21:41:01 -0700

    Initial Commit

---
+++ UniMentor/UniMentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniMentorApp.swift
+//  UniMentor
+//
+//  Created by Son Minh Nguyen on 2022-05-19.
+//
+
+import SwiftUI
+
+@main
+struct UniMentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
