774e8b8c0c5aa85ffff533ff2562bb56bedc8966
CSTNicole <000431652@student.vcc.ca>
2022-06-03 22:00:54 -0700

    done tab bar view, added new files for other project sections

---
+++ UniMentor/view/screens/RequestsView.swift
@@ -0,0 +1,20 @@
+//
+//  RequestsView.swift
+//  UniMentor
+//
+//  Created by Mary Nicolette Tan on 2022-06-03.
+//
+
+import SwiftUI
+
+struct RequestsView: View {
+    var body: some View {
+        Text("This is the REQUESTS VIEW")
+    }
+}
+
+struct RequestsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RequestsView()
+    }
+}
