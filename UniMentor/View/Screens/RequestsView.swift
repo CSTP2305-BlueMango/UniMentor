96a7a7eb8ad28021aaff79fbf212d11f2ab029cb
YoonseoJ <cassie961206@gmail.com>
2022-06-05 00:05:09 -0700

    HomeView, RequestView, MatchedView, ChatView

--- UniMentor/View/Screens/RequestsView.swift
+++ UniMentor/View/Screens/RequestsView.swift
@@ -10,15 +10,66 @@ import SwiftUI
 struct RequestsView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                Text("This is the REQUESTS VIEW")
-                VStack(alignment: .center) {
-                    NavigationLink(destination: RequestsProfileView()) {
-                        Text("To REQUESTS profile")
+            // BODY
+            VStack(spacing: UIScreen.main.bounds.height * 0.02) {
+                // HEADER
+                HStack {
+                    Text("Requests")
+                        .font(Font.custom("Charm-Regular", size: UIScreen.main.bounds.width * 0.12))
+                    Spacer()
+                } //: HEADER
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.05)
+                Spacer()
+                // MAIN
+                VStack(spacing: UIScreen.main.bounds.height * 0.04) {
+                    // Card list
+                    // TODO: make it scrollable
+                    VStack(spacing: UIScreen.main.bounds.height * 0.02) {
+                        // TODO: this is gonna be For loop
+                        NavigationLink(destination: RequestsProfileView()) {
+                            ButtonCardView(
+                                color: Color(red: 0.8627, green: 0.8313, blue: 0.7960),
+                                buttonText: "Accept"
+                            )
+                        }
+                        NavigationLink(destination: RequestsProfileView()) {
+                            ButtonCardView(
+                                color: Color(red: 0.8627, green: 0.8313, blue: 0.7960),
+                                buttonText: "Accept"
+                            )
+                        }
+                        NavigationLink(destination: RequestsProfileView()) {
+                            ButtonCardView(
+                                color: Color(red: 0.8627, green: 0.8313, blue: 0.7960),
+                                buttonText: "Accept"
+                            )
+                        }
+                        NavigationLink(destination: RequestsProfileView()) {
+                            ButtonCardView(
+                                color: Color(red: 0.8627, green: 0.8313, blue: 0.7960),
+                                buttonText: "Accept"
+                            )
+                        }
+                        NavigationLink(destination: RequestsProfileView()) {
+                            ButtonCardView(
+                                color: Color(red: 0.8627, green: 0.8313, blue: 0.7960),
+                                buttonText: "Accept"
+                            )
+                        }
                     }
-                }
-            }
-        }
+                    Spacer()
+                } //: MAIN
+                
+//                Text("This is the REQUESTS VIEW")
+//                VStack(alignment: .center) {
+//                    NavigationLink(destination: RequestsProfileView()) {
+//                        Text("To REQUESTS profile")
+//                    }
+//                }
+            } //: BODY
+            .frame(height: UIScreen.main.bounds.height * 0.95)
+            Spacer()
+        } //: NAV_VIEW
     }
 }
 
